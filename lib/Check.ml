c472e359568e4c77531e00fc446b265072e4c222
Lîm Tsú-thuàn <dannypsnl@gmail.com>
2022-09-05 19:43:21 +0800

    extract bidirectional check

--- lib/Check.ml
+++ lib/Check.ml
@@ -3,6 +3,9 @@ open Type
 open Ast
 module Map = Hashtbl
 
+(* TODO: conversion check
+   This module should return a really to run code! *)
+
 (* global context bounds
    ---------------------
       string => type *)
@@ -14,12 +17,18 @@ let rec check_top : context -> top -> unit =
  fun ctx tm ->
   match tm with
   | Let (x, ty, e) ->
-      let ty' = infer_ty ctx e in
-      equate_ty ty ty';
+      check_tm ctx e ty;
       print_string ("[ok] let " ^ x ^ ": checked");
       print_newline ();
       Map.add ctx x ty
 
+(* check term has type *)
+and check_tm : context -> term -> typ -> unit =
+ fun ctx tm ty ->
+  let ty' = infer_ty ctx tm in
+  equate_ty ty ty'
+
+(* synthesize type from term *)
 and infer_ty : context -> term -> typ =
  fun ctx t ->
   match t with
