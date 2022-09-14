c6a90b124e1da1fe2a79d643dd7fd589cb51d701
Lîm Tsú-thuàn <dannypsnl@gmail.com>
2022-09-14 14:31:39 +0800

    conversion check
    prebind non-mono before check it
    
    Co-authored-by: cybai (Haku) <cyb.ai.815@gmail.com>
    Signed-off-by: Lîm Tsú-thuàn <dannypsnl@gmail.com>

--- lib/Conversion.ml
+++ lib/Conversion.ml
@@ -11,15 +11,20 @@
 open Type
 open Ast
 open Check
+module Map = Hashtbl
 
-let isMono : top -> bool =
- fun t ->
-  match t with Let (_, Arrow (_, _), _) -> false | Let (_, _, _) -> true
+let isMono : context -> top -> bool =
+ fun ctx t ->
+  match t with
+  | Let (x, (Arrow (_, _) as ty), _) ->
+      Map.add ctx x ty;
+      false
+  | Let (_, _, _) -> true
 
 let conversion : program -> program =
  fun p ->
   let init_ctx = empty_context in
-  let monos, non_monos = List.partition isMono p in
+  let monos, non_monos = List.partition (isMono init_ctx) p in
   check_all init_ctx non_monos;
   check_all init_ctx monos;
   List.append non_monos monos
